96de826c3ad8adc2d81edfa531d47be8ce5143be
Lars Rickert <dev@lars-rickert.de>
2023-09-04 20:56:45 +0200

    chore: add app title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   css: ["~/assets/styles/index.scss"],
   app: {
     head: {
+      title: "Webentwicklung Lars Rickert",
       link: [{ rel: "icon", href: "/favicon.svg" }],
       htmlAttrs: {
         class: "dark",
