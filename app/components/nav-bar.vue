275de97b6d96f195ed6b5024b4f784276cb85e97
Lars Rickert <dev@lars-rickert>
2026-06-20 22:56:57 +0200

    update nav bar

--- app/components/nav-bar.vue
+++ app/components/nav-bar.vue
@@ -1,4 +1,7 @@
 <script lang="ts" setup>
+import iconGitHub from "~/assets/icons/github.svg?raw";
+import iconLinkedIn from "~/assets/icons/linkedin.svg?raw";
+
 const localePath = useLocalePath();
 </script>
 
@@ -9,6 +12,19 @@ const localePath = useLocalePath();
 
     <template #globalContextArea>
       <ColorSchemeSwitch />
+      <OnyxSeparator orientation="vertical" />
+      <OnyxUnstableNavButton
+        label="GitHub"
+        :icon="iconGitHub"
+        hide-label
+        :link="{ href: 'https://github.com/larsrickert', target: '_blank' }"
+      />
+      <OnyxUnstableNavButton
+        label="LinkedIn"
+        :icon="iconLinkedIn"
+        hide-label
+        :link="{ href: 'https://www.linkedin.com/in/lars-rickert-392857255', target: '_blank' }"
+      />
     </template>
   </OnyxNavBar>
 </template>
@@ -20,5 +36,17 @@ const localePath = useLocalePath();
 
 :deep(.onyx-nav-bar__nav > ul) {
   justify-content: flex-end;
+}
+
+:deep(.onyx-nav-bar__mobile-global-context) {
+  .onyx-separator {
+    height: var(--onyx-separator-min-size);
+    width: var(--onyx-separator-size);
+    min-width: unset;
+  }
+}
+
+:deep(.onyx-nav-item--navbar) {
+  font-size: 0.875rem;
 }
 </style>
